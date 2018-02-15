ac4c383bd8fd0f68cccd05f38d21645bcfad88b8
burchill <zach.burchill+gh@gmail.com>
2018-02-15 01:06:00 -0500

    added package-wide doc

---
+++ R/zplyr.R
@@ -0,0 +1,20 @@
+#' zplyr: Zachary Burchill's personal package
+#'
+#' \strong{The zplyr package has nothing to do with "plyr" or "dplyr",
+#' the name was just a bad joke.} \cr \cr
+#' zplyr is a collection of personal functions that I,
+#' \href{http://burchill.github.io}{Zach Burchill}, have found useful in my grad
+#' studies. This package was created before the big \code{tidyverse} update,
+#' when it got all that cool non-standard evaluation \code{rlang} stuff that makes it
+#' so easy to jive with the rest of the \code{tidyverse}. \cr \cr
+#' Furthermore, much of this code was written early in my grad career, when I wasn't
+#' as familiar with what \code{R} packages were out there, and when I pretty
+#' much sucked. I did learn a lot about \code{nse} and \code{R} documentation and
+#' package creation making this, however.
+#'
+#' @section Foo functions:
+#' The foo functions ...
+#'
+#' @docType package
+#' @name zplyr
+NULL
