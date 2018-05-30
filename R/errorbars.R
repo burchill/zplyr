3d911fabdff05623d66087570454e621a04d1610
burchill <zach.burchill+gh@gmail.com>
2018-05-29 22:53:58 -0400

    fixed naming and updated version and license

--- R/errorbars.R
+++ R/errorbars.R
@@ -1,7 +1,7 @@
-#' (Deprecated) Make ggplot errorbars and mean with a single function
+#' (Deprecated) Make ggplot error bars and mean with a single function
 #'
-#' \strong{Deprecated! Use \code{\link{geom_errorbars}} instead!} \cr
-#' Often, automatically bootstrapped errorbars in \code{ggplot2} are accompanied
+#' \strong{Deprecated! Use \code{\link{stat_errorbar}} instead!} \cr
+#' Often, automatically bootstrapped error bars in \code{ggplot2} are accompanied
 #' by a dot indicating the mean of the errorbars. Generally, this is done with
 #' two calls of \code{\link[ggplot2]{stat_summary}}, one for the bars and one
 #' for the dot.  This function is just a shortcut that will add both.
@@ -17,7 +17,7 @@
 #' }
 #' @export
 errorbars <- function(gg_obj, ...) {
-  .Deprecated("geom_errorbars")
+  .Deprecated("stat_errorbar")
   ggplot2::`%+%`(
     ggplot2::`%+%`(
       gg_obj,
@@ -28,13 +28,13 @@ errorbars <- function(gg_obj, ...) {
 }
 
 
-#' (Deprecated)Make dotplots with errorbars with a single function
+#' (Deprecated) Make dotplots with error bars with a single function
 #'
-#' \strong{Deprecated! Use \code{\link{geom_dots_and_bars}} instead!} \cr
-#' Adds a dotplot, 95\% CI errorbars, and a point representing the mean
+#' \strong{Deprecated! Use \code{\link{stat_dots_and_bar}} instead!} \cr
+#' Adds a dotplot, 95\% CI error bars, and a point representing the mean
 #' to a ggplot object. Almost identical to \code{\link{errorbars}}, but
 #' with the obvious additional \code{\link[ggplot2]{geom_dotplot}}. \cr
-#' Right now, only uses default values for errorbars
+#' Right now, only uses default values for error bars
 #'
 #' @param gg_obj The ggplot object (i.e., what is built up from \code{ggplot(\dots)}). If \code{errorbars()} does not immediately follow \code{ggplot(\dots)}, use \code{\%+\%} instead of \code{+} to add the intervening ggplot layers until the ggplot object is piped into the function.
 #' @param \dots Additional arguments for the \code{\link[ggplot2]{stat_summary}} pair.
@@ -51,7 +51,7 @@ dots_and_bars <-function(gg_obj,
                         dotsize = 0.5,
                         alpha=0.2,
                         ...) {
-  .Deprecated("geom_dots_and_bars")
+  .Deprecated("stat_dots_and_bar")
   errorbars(
     ggplot2::`%+%`(
       gg_obj,
--- R/errorbars_new.R
+++ R/errorbars_new.R
@@ -1,9 +1,14 @@
-#' Make ggplot errorbars and mean with a single function
+#' Make ggplot error bars and mean with a single function
 #'
-#' Often, automatically bootstrapped errorbars in \code{ggplot2} are accompanied
+#' Often, automatically bootstrapped error bars in \code{ggplot2} are accompanied
 #' by a dot indicating the mean of the errorbars. Generally, this is done with
 #' two calls of \code{\link[ggplot2]{stat_summary}}, one for the bars and one
-#' for the dot.  This function is just a shortcut that will add both.
+#' for the dot.  This function is just a shortcut that will add both. 
+#' 
+#' \code{geom_errorbars} is the deprecated name for \code{stat_errorbar}. I realized that
+#' \code{ggplot2} already had a \code{\link[ggplot2]{geom_errorbar}}, which would be very 
+#' confusing. Additionally, since \code{geom_errorbars} operated more like a `stat` function 
+#' (e.g., `stat_summary`), it made more sense to change its name.
 #'
 #' @param \dots Additional arguments for the \code{\link[ggplot2]{stat_summary}} pair.
 #' @return A list of the \code{stat_summary} objects
@@ -15,22 +20,41 @@
 #'
 #'ggplot(df, aes(x = x, y = y)) +
 #'   geom_point(alpha=0.3) +
-#'   geom_errorbars(color="red") +
+#'   stat_errorbar(color="red") +
 #'   xlab("WHAAAT")
-#' @seealso \code{\link{geom_dots_and_bars}}
+#' @seealso \code{\link{stat_dots_and_bar}}
+#' @export
+stat_errorbar <- function(...) {
+  return(list(ggplot2::stat_summary(fun.data = "mean_cl_boot", geom = "errorbar", ...),
+              ggplot2::stat_summary(fun.y = mean, geom = "point", ...)))
+}
+
+#' @rdname stat_errorbar
 #' @export
 geom_errorbars <- function(...) {
+  .Deprecated("stat_errorbar")
   return(list(ggplot2::stat_summary(fun.data = "mean_cl_boot", geom = "errorbar", ...),
-              ggplot2::stat_summary(fun.y=mean, geom = "point", ...)))
+              ggplot2::stat_summary(fun.y = mean, geom = "point", ...)))
 }
 
 
-#' Make dotplots with errorbars with a single function
+
+
+
+#' Make dotplots with error bars with a single function
 #'
-#' Adds a dotplot, 95\% CI errorbars, and a point representing the mean
-#' to a ggplot object. Almost identical to \code{\link{errorbars}}, but
+#' Adds a dotplot, 95\% CI error bars, and a point representing the mean
+#' to a ggplot object. Almost identical to \code{\link{stat_errorbar}}, but
 #' with the obvious additional \code{\link[ggplot2]{geom_dotplot}}. \cr
-#' Right now, only uses default values for errorbars
+#'  \cr
+#' Right now, only uses default values for the error bars.
+#' 
+#' \code{geom_dots_and_bars} is the deprecated name for \code{stat_dots_and_bar}. I realized that
+#' \code{ggplot2} already had a \code{\link[ggplot2]{geom_errorbar}}, which would be very 
+#' confusing for \code{geom_dots_and_bars}'s sister function, \code{\link{geom_errorbars}}.
+#' To keep the naming similarity, and because \code{geom_dots_and_bars} operated more 
+#' like a `stat` function (e.g., `stat_summary`) than a `geom`, it made 
+#' more sense to change its name.
 #'
 #' @param \dots Additional arguments for the \code{\link[ggplot2]{stat_summary}} pair.
 #' @return A list of the \code{stat_summary} objects
@@ -41,19 +65,35 @@ geom_errorbars <- function(...) {
 #' )
 #'
 #' ggplot(df, aes(x = x, y = y)) +
-#'   geom_dots_and_bars(color="red",fill="red") +
+#'   stat_dots_and_bar(color="red",fill="red") +
 #'   xlab("WHAAAT")
-#' @seealso \code{\link{errorbars}}
+#' @seealso \code{\link{stat_errorbar}}
 #' @export
-geom_dots_and_bars <-function(...,
-                         binaxis = "y",
-                         stackdir = "center",
-                         dotsize = 0.5,
-                         alpha=0.2) {
+stat_dots_and_bar <- function(...,
+                               binaxis = "y",
+                               stackdir = "center",
+                               dotsize = 0.5,
+                               alpha=0.2) {
   dotplot <- list(ggplot2::geom_dotplot(binaxis = binaxis,
                                         stackdir = stackdir,
                                         dotsize = dotsize,
                                         alpha=alpha,
                                         ...))
-  return(append(dotplot, geom_errorbars(...)))
+  return(append(dotplot, stat_errorbar(...)))
+}
+
+#' @rdname stat_dots_and_bar
+#' @export
+geom_dots_and_bars <- function(...,
+                               binaxis = "y",
+                               stackdir = "center",
+                               dotsize = 0.5,
+                               alpha=0.2) {
+  .Deprecated("stat_dots_and_bar")
+  dotplot <- list(ggplot2::geom_dotplot(binaxis = binaxis,
+                                        stackdir = stackdir,
+                                        dotsize = dotsize,
+                                        alpha=alpha,
+                                        ...))
+  return(append(dotplot, stat_errorbar(...)))
 }
