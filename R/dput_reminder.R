f5712d8796fcb9f3fbc7ad6f568d668dc5b9c947
burchill <zach.burchill+gh@gmail.com>
2018-09-01 15:39:15 -0400

    added note about dput

---
+++ R/dput_reminder.R
@@ -0,0 +1,8 @@
+#' How to turn a object into a text representation/input
+#'
+#' This isn't a function---it's just a note to document something that I found to be very useful. \cr
+#'  \cr
+#' Just use `dput(x)`. E.g., if `x` is `c(1,2,3)`, then `dput(x)` is "c(1, 2, 3)".  Thats it!
+#'
+#' @name object_to_input
+NULL
