5e5707ecdec21b8d7ee69c243e5a8df694dc785e
burchill <zach.burchill+gh@gmail.com>
2018-09-01 15:38:50 -0400

    fixed typos in floating latex note

--- R/stop_floating_in_latex_note.R
+++ R/stop_floating_in_latex_note.R
@@ -6,19 +6,19 @@
 #' so it doesn't float past where I want it in the document. Using `knitr`'s hooks, I can make this
 #' process happen automatically. \cr
 #'  \cr
-#' In order to do that, you first need to include the LaTeX package `placeins` 
-#' (i.e., via `header-includes: \\usepackage{placeins}`). Then, in the first chunk, you 
+#' In order to do that, you first need to include the LaTeX package `placeins`
+#' (i.e., via `header-includes: \\usepackage{placeins}`). Then, in the first chunk, you
 #' should put the function: \cr
 #' ```
 #' knitr::knit_hooks$set(plot = function(x, options)  {
 #'   if (is.null(options$regfloat) || options$regfloat==FALSE) {
-#'     paste0(knitr::hook_plot_tex(x, options), "\n\\FloatBarrier\n")
+#'     paste0(knitr::hook_plot_tex(x, options), "\n\\\\FloatBarrier\n")
 #'   } else {
 #'     knitr::hook_plot_tex(x, options)
 #'   }
 #' })
 #' ```
 #' In order to disable this behavior for specific chunks, just put `regFloat=TRUE` as a chunk option.
-#' 
+#'
 #' @name stop_floating_in_latex
 NULL
